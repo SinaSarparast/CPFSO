c92d53c35611320f2ba577e97d636a30bed899d7
MariamCook <mariam.cook@soton.ac.uk>
2018-04-25 23:19:24 +0100

    Scrape BBCFoodwithCuisines save json 11 pagerecipes per cuisine 846 total- ready to get more

--- JupyterNotebook/Dataset/ScrapeRecipeWebsites/getdata-bbcgoodfoodwithcuisines.py
+++ JupyterNotebook/Dataset/ScrapeRecipeWebsites/getdata-bbcgoodfoodwithcuisines.py
@@ -112,18 +112,80 @@ for linkto_recipe in recipe_page_urls:
         print(makejsonready)
         
         listforjsonoutput.append(makejsonready)
-    mycuisine_index=mycuisine_index+1
-                
+
+
+print("now I am going to the next page of recipes for each cuisine, and getting their recipes, this will take a while!!!")
+
+#scrape 10 more pages max
+pagestocrawl = [2,3,4,5,6,7,8,9,10,11]
+
+
+for recipe_in_list in selectedcuisinesbbcfood:
+    for anumber in pagestocrawl:
+        try:
+            build_additional_recipes_url = "https://www.bbc.com/food/recipes/search?page="+str(anumber)+"&cuisines%5B0%5D="+recipe_in_list+"&sortBy=lastModified"
+            print(build_additional_recipes_url)
+            
+            print("I am going to this url now %s" % build_additional_recipes_url)
+            (webpage,alink)=geturlcontents(build_additional_recipes_url)
+            print("I have collected the food links page from  %s" % alink)
+            #print(webpage)
+            
+            print("i am using BeautifulSoup to parse the recipe index page")
+            soup = BeautifulSoup(webpage, "html.parser")
+            
+            print("I made some soup with the food links page")
+            
+            linkstocrawl=[];
+            for h3inrecipespage in soup.findAll("h3", class_=""):
+                print("i found a h3 %s" % h3inrecipespage)
+                for a in h3inrecipespage.findAll("a", href=True):
+                    print("got a link woohoo!!")
+                    print(a['href'])
+                    finalrecipecombinedlink = "https://www.bbc.com" + a['href']
+                    print(finalrecipecombinedlink)
+                    linkstocrawl.append(finalrecipecombinedlink)
+            
+            print("here are all my links to crawl")
+            print(linkstocrawl)
+            
+            print("going to get the recipes now")
+            
+            for recipe_page in linkstocrawl:
+                print("I am going to this recipe page now %s" % recipe_page)
+                (webpage,alink)=geturlcontents(recipe_page)
+                print("I have collected the food links page from  %s" % alink)
+                print("i am using BeautifulSoup to parse the recipe page")
+                soup = BeautifulSoup(webpage, "html.parser")
+                for h1found in soup.findAll("h1"):
+                    print("got a recipe name woohoo!!")
+                    print(h1found.text)
+                ingredients_for_recipe=[];
+                for li in soup.findAll("li", class_="recipe-ingredients__list-item"):
+                    print("got an ingredient woohoo!!")
+                    print(li)
+                    print(li.text)
+                    ingredients_for_recipe.append(li.text)
+                makejsonready = {u"id": h1found.text, u"cuisine": selectedcuisinesbbcfood[mycuisine_index], u"ingredients": ingredients_for_recipe}
+                print(makejsonready)
+        
+                listforjsonoutput.append(makejsonready)
+
+        except: 
+            print("I cannot find another recipe index page, sorry!")
+
+print("that's it I have done all the recipes woohoo!!!!!!!!!!!!!!!")
+#mycuisine_index=mycuisine_index+1
+             
 print("here is the json")
 print(listforjsonoutput)
 
 
 print("woohoo!!!!! I am writing all the recipe data to the file")
   
-with io.open('bbcfoodcuisines-sample.json', 'w', encoding='utf-8') as f:
+with io.open('bbcfoodcuisines-10pages.json', 'w', encoding='utf-8') as f:
     json.dump(listforjsonoutput, f, ensure_ascii=False, indent=4)
 
 print("Total recipes saved:")
 print(len(listforjsonoutput))
-   
    
